c43b886155a91f2495bed7a9662c0005b1f8704e
YooSeonHo <tjsgh503@naver.com>
2021-12-06 13:14:27 +0900

    Router-dom 사용해보기

--- practice/src/components/Movie.js
+++ practice/src/components/Movie.js
@@ -1,10 +1,13 @@
-import propTypes from "prop-types"
+import propTypes from "prop-types";
+import {Link} from 'react-router-dom';
 
 function Movie({coverImg, title, summary, genres}){
     return(
         <div>
           <img src={coverImg} alt={title}/>
-          <h2>{title}</h2>
+          <h2>
+            <Link to='/movie'> {title} </Link>
+            </h2>
           <p>{summary}</p>
           <ul>
             {genres.map((g) => (<li key={g}>{g}</li>))}
@@ -12,7 +15,7 @@ function Movie({coverImg, title, summary, genres}){
         </div>
     );
 }
-export default Movie;
+
 
 Movie.propTypes = {
     coverImg: propTypes.string.isRequired,
@@ -20,3 +23,5 @@ Movie.propTypes = {
     summary:propTypes.string.isRequired,
     genres:propTypes.arrayOf(propTypes.string).isRequired,
 };
+
+export default Movie;
